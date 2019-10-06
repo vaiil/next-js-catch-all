6741f52c416cf820a0e44dcc30f4a0039337e4c6
vail <ysywix@gmail.com>
2019-10-07 01:42:03 +0300

    Add patch for catch-all routes

--- dist/next-server/lib/router/utils/route-regex.js
+++ dist/next-server/lib/router/utils/route-regex.js
@@ -5,10 +5,20 @@ function getRouteRegex(normalizedRoute) {
     const escapedRoute = (normalizedRoute.replace(/\/$/, '') || '/').replace(/[|\\{}()[\]^$+*?.-]/g, '\\$&');
     const groups = {};
     let groupIndex = 1;
-    const parameterizedRoute = escapedRoute.replace(/\/\\\[([^\/]+?)\\\](?=\/|$)/g, (_, $1) => ((groups[$1
+    let parameterizedRoute = escapedRoute.replace(/\/\\\[([^\/]+?)\\\](?=\/|$)/g, (_, $1) => ((groups[$1
         // Un-escape key
         .replace(/\\([|\\{}()[\]^$+*?.-])/g, '$1')] = groupIndex++),
         '/([^/]+?)'));
+    const groupNames = Object.keys(groups);
+    const lastGroup = groupNames[groupNames.length - 1];
+    if (lastGroup && lastGroup[lastGroup.length - 1] === '*') {
+        parameterizedRoute =
+            parameterizedRoute.slice(0, parameterizedRoute.lastIndexOf('([^/]+?)')) +
+                '(.*)';
+        const value = groups[lastGroup];
+        delete groups[lastGroup];
+        groups[lastGroup.slice(0, -1)] = value;
+    }
     return {
         re: new RegExp('^' + parameterizedRoute + '(?:/)?$', 'i'),
         groups,
